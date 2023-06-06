e6b4fd4b887331e598af51bcd78450319689a31d
DILIPREDDYAV <109126436+DILIPREDDYAV@users.noreply.github.com>
2023-06-06 20:04:14 +0530

    Add files via upload

---
+++ src/Requests.js
@@ -0,0 +1,11 @@
+const key = '23a9434c5c88de248b4b565b7dbf2827'
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+  };
+
+  export default requests
